b208cb7697a513ba57bbdae8f18e32057472614d
Prince Mbeah Essilfie <pmessilfie@st.knust.edu.gh>
2023-03-23 05:57:18 -0700

    Update Assessment.java

--- Assessment.java
+++ Assessment.java
@@ -1,29 +1,29 @@
-import java.util.Scanner;
-
-public class Assessment {
-    public static void main(String [] args){
-        int examScore, assessmentScore, fees;
-
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter the exam score: ");
-        examScore = keyboard.nextInt();
-
-        System.out.print("Enter the assessment score: ");
-        assessmentScore = keyboard.nextInt();
-
-        System.out.print("Enter the amount of fees paid: ");
-        fees = keyboard.nextInt();
-
-        int exam_div = examScore/10, exam_rem = examScore % 10;
-        int assessment_div = assessmentScore/10, assessment_rem = assessmentScore % 10;
-         
-        switch (examScore ) {
-            case 4:
-                break;
-        
-            default:
-                break;
-        }
-        
-    }
-}
+import java.util.Scanner; 
+public class EssilfiePrinceMbeah { 
+   public static void main(String[] args) { 
+   double examScore, assessmentScore, fees; 
+boolean hasPassed = false, certified = false; 
+Scanner keyboard = new Scanner(System.in); 
+System.out.print("Enter the exam score: "); 
+//receive the exam score as a double
+examScore = keyboard.nextDouble(); 
+System.out.print("Enter the assessment score: "); 
+//receive the assessment score as a double
+assessmentScore = keyboard.nextDouble(); 
+System.out.print("Enter the amount of fees paid: "); 
+//receive the fees paid as a double fees = 
+keyboard.nextDouble(); 
+if (examScore >= 25) hasPassed = true; 
+if (assessmentScore >= 15) certified = true; 
+if ((examScore == 25 && assessmentScore == 14) || (examScore == 24 &&
+assessmentScore == 15)){ 
+ hasPassed = true; certified = true; 
+} 
+if (hasPassed) System.out.print("You passed the exams"); 
+else System.out.print("You failed the exams"); 
+if (certified) System.out.print(" and passed the assessment."); 
+else System.out.print(" and failed the assesment."); 
+if (hasPassed && certified && fees == 100) System.out.print(" You can now be 
+given a certificate"); 
+else System.out.print(" You can't be given a certificate."); 
+if (!hasPassed &
